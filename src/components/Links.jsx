c6140098a934d9c7ec702cbb278c11c721162725
FelipeTRobaina <felipe.t.robaina@gmail.com>
2025-12-19 01:21:59 -0300

    Projeto recuperado e configurado

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,8 +1,6 @@
 function Links() {
   return (
     <div className="botoes">
-        <a href="https://google.com" target="_blank" rel="noreferrer">Inscreva-se na Disciplina</a>
-        <a href="https://google.com" target="_blank" rel="noreferrer">Baixe os materiais</a>
         <a href="https://google.com" target="_blank" rel="noreferrer">Veja meu Portfólio</a>
         <a href="https://google.com" target="_blank" rel="noreferrer">Sobre mim</a>
     </div>
